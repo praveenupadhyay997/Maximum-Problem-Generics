361d12259e7fd9359547af7fe554789ad95f4dfe
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-21 00:24:09 +0530

    [Praveen] Add: Added the functionality to compare strings lexicographically and return the maximum string out of them.

--- MaximumProblemGenerics/MaximumProblemGenerics/MaximumClass.cs
+++ MaximumProblemGenerics/MaximumProblemGenerics/MaximumClass.cs
@@ -52,5 +52,26 @@ namespace MaximumProblemGenerics
             else
                 return 0;
         }
+
+        /// <summary>
+        /// Function to find the maximum among passed three string values lexicographically
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns></returns>
+        public static string MaximumAmongThree(string first, string second, string third)
+        {
+            //Use of Compare to function so as to compute the maximum values on basis of difference lexicographically
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                return first;
+            else if (second.CompareTo(third) > 0 && second.CompareTo(first) > 0)
+                return second;
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+                return third;
+            //Returning zero in case user has passed same number for all three
+            else
+                return "Same";
+        }
     }
 }
