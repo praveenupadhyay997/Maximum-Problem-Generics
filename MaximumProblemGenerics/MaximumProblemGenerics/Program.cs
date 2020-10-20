b46a9ed0ccead72cd3b78bf9750328911f78aa03
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-20 22:16:18 +0530

    [Praveen] Add: To compute the maximum among three integral number passed in the main method.

---
+++ MaximumProblemGenerics/MaximumProblemGenerics/Program.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Praveen Kumar Upadhyay"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace MaximumProblemGenerics
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======================================");
+            Console.WriteLine("Welcome to Maximum Computation Problem");
+            Console.WriteLine("======================================");
+            int maximumNumber = MaximumClass.MaximumIntegerNumberAmongThree(10, 30, 20);
+            if (maximumNumber == 0)
+                Console.WriteLine("All three Numbers are equal");
+            else
+                Console.WriteLine("Maximum Number =" + maximumNumber);
+        }
+    }
+}
