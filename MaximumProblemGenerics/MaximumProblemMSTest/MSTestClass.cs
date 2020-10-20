87d319c0e90153e0a739c751e70b08dc0c5c45bb
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-20 22:38:17 +0530

    [Praveen] Add: Implemented the second use case passing the maximum number at second position.

--- MaximumProblemGenerics/MaximumProblemMSTest/MSTestClass.cs
+++ MaximumProblemGenerics/MaximumProblemMSTest/MSTestClass.cs
@@ -10,6 +10,8 @@ namespace MaximumProblemMSTest
         [TestCategory("Maximum Check For Integer")]
         // TC 1.1- Giving the maximum number at first position and comparing with the max number returned from maximum function
         [DataRow(30, 20, 10)]
+        // TC 1.2- Giving the maximum number at second position and comparing with the max number returned from maximum function
+        [DataRow(20, 30, 10)]
         [TestMethod]
         public void TestMethodForIntegralInputs(int first, int second, int third)
         {
@@ -23,6 +25,16 @@ namespace MaximumProblemMSTest
                 //Assert
                 Assert.AreEqual(expected, actual);
             }
-         }
+            else if (second == 30)
+            {
+                //Arrange
+                maximumClass = new MaximumClass();
+                //Act
+                int expected = second;
+                int actual = MaximumClass.MaximumIntegerNumberAmongThree(first, second, third);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
     }
 }
